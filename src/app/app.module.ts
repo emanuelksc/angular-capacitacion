89b20a3d6929bfc5d55c45ee1c92cdee90699e7a
Emanuel Samaniego Calderón <35107738+emanuelksc@users.noreply.github.com>
2023-01-06 22:40:28 +0000

    Prueba en Replit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
-import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +10,7 @@ import { PagesModule } from './pages/pages.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ComponentsModule,
-    PagesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
